50c27c993940f838e9b5d6d4dbcd25d3a76d7bd1
augusto-mattos <guto.s.mattos@gmail.com>
2023-06-05 11:09:38 +0200

    Component integration

---
+++ functions/portas.ts
@@ -0,0 +1,21 @@
+import PortaModel from "../model/porta";
+
+export default function criarPortas(qtde: number, selecionada: number): PortaModel []{
+    return Array.from({ length: qtde }, (_, i) => {
+        const numero = i + 1
+        const presente = numero === selecionada
+        return new PortaModel(numero, presente)
+    })
+}
+
+export function atualizarPortas(portas: PortaModel[], portaModificada: PortaModel): PortaModel[] {
+    return portas.map(portaAtual => {
+        const igualAModificada = portaAtual.numero === portaModificada.numero
+
+        if(igualAModificada) {
+            return portaModificada
+        } else {
+            return portaModificada.aberta ? portaAtual : portaAtual.desselecionar()
+        }
+    })
+}
